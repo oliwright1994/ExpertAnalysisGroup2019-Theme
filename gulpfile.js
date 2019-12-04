36059dfa42bc368294590bc2db906f03291737cf
Oli Wright <oli.wright@blueyonder.co.uk>
2019-12-04 14:59:11 +0000

    added dev dependancies and gulp workflow

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync");
+var reload = browserSync.reload;
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+
+// browser-sync task for starting the server.
+gulp.task("browser-sync", function() {
+  //watch files
+  var files = ["./sass/*.scss", "./*.php"];
+
+  //initialize browsersync
+  browserSync.init(files, {
+    //browsersync with a php server
+    proxy: "http://one.wordpress.test",
+    notify: true
+  });
+});
+
+// Sass task, will run when any SCSS files change & BrowserSync
+// will auto-update browsers
+gulp.task("sass", function() {
+  return gulp
+    .src(["sass/main.scss", "sass/*.scss"])
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(concat("styles.css"))
+    .pipe(gulp.dest("./css"))
+    .pipe(reload({ stream: true }));
+});
+
+// Default task to be run with `gulp`
+gulp.task(
+  "default",
+  gulp.series("sass", "browser-sync", function() {
+    gulp.watch("sass/**/*.scss", ["sass"]);
+  })
+);
